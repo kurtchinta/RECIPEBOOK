f34501075b5c977f691b47ab99b98df0657e9902
kurtchinta <blue09908@gmail.com>
2024-11-30 01:50:46 +0800

    naa ra sa resources/pages and ui tiwasa ang home and other page

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,14 +4,14 @@ import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <Head title="Home" />
+    <Head title="Dashboard" />
 
     <AuthenticatedLayout>
         <template #header>
             <h2
                 class="text-xl font-semibold leading-tight text-gray-800"
             >
-                Home
+                Dashboard
             </h2>
         </template>
     </AuthenticatedLayout>
